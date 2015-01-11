e904f2798e22d47e4c4ad8b3f292ef208124c020
Jamshed Vesuna <jamshed@chronos.local>
2015-01-11 00:45:03 -0800

    Install packages

--- modules/people/manifests/JamshedVesuna.pp
+++ modules/people/manifests/JamshedVesuna.pp
@@ -12,4 +12,30 @@ class people::jamshedvesuna {
       source  => 'altoduo/dotfiles',
       require => File[$scripts];
   }
+
+  include spotify
+  include dropbox
+  include keepassx
+  include macvim
+  include virtualbox
+  include python
+  include wget
+
+  package {
+    [
+      'rdiff-backup',
+      'awscli',
+      'ctags',
+      'the_silver_searcher',
+    ]:
+      ensure => installed,
+      provider => 'homebrew';
+  }
+
+  git::config::global {
+    'user.name':
+      value => 'JamshedVesuna';
+    'user.email':
+      value => 'jamshed.vesuna@gmail.com';
+  }
 }
